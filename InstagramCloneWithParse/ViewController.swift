39c5626d29683b895ca0b05e7d07e465ae087de4
fbutev <fazil.butev@assistt.com.tr>
2022-09-19 11:41:45 +0300

    Initial Commit

---
+++ InstagramCloneWithParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCloneWithParse
+//
+//  Created by sys on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
